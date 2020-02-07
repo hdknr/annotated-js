e4ffa2fb3a84a0782774a69f1348ad1f9e8da38f
user.name <gmail@hdknr.com>
2020-02-07 15:27:55 +0900

    React Hooks: useStatte

--- samples/helloreact/src/App.js
+++ samples/helloreact/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +20,10 @@ function App() {
         >
           Learn React
         </a>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
       </header>
     </div>
   );
